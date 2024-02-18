14ead635d5588112c7dfb3de3cb5e3615c6e8a13
SriVarsha99 <srivarsha.gowdara@gmail.com>
2024-02-17 16:36:01 -0800

    Firebase_setup

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+import firebase_admin
+from firebase_admin import db,credentials
+
+cred = credentials.Certificate("firebase_cred.json")
+firebase_admin.initialize_app(cred, {"databaseURL": "https://h4h2024-300a6-default-rtdb.firebaseio.com/"})
+
 app = Flask(__name__)
 CORS(app)
 
@@ -19,3 +25,5 @@ def login():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+db.reference("/user/Srivarsha_GC").update({"password":"Var","events":"abc"})
